3dd80322d7071e28a630c5c67086fe65a1457e3f
s-en <sen@nemusoft.com>
2022-02-05 10:57:03 +0900

    ぷよ碁サイトへ修正

--- site.config.js
+++ site.config.js
@@ -1,25 +1,24 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
-
+  rootNotionPageId: 'a4c0f1aa2f9e4f92ab6628a4339cef3a',
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'ぷよ碁 開発ブログ',
+  domain: 'blog.puyogo.app',
+  author: 'puyogo.app',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '初心者向け囲碁ブラウザゲーム「ぷよ碁」の開発状況やTipsをまとめています',
+  socialImageTitle: 'ぷよ碁 開発ブログ',
+  socialImageSubtitle: '初心者向け囲碁ブラウザゲーム「ぷよ碁」の開発状況やTipsをまとめています',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'sen9937510',
+  github: 's-en',
+  //linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
