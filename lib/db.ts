1def69310be093ad060c31db4a7d2ba6fbb89941
s-en <sen@nemusoft.com>
2022-02-08 22:35:47 +0900

    test5

--- lib/db.ts
+++ lib/db.ts
@@ -4,11 +4,11 @@ import * as config from './config'
 export let db: firestore.Firestore = null
 export let images: firestore.CollectionReference = null
 
-if (config.isPreviewImageSupportEnabled) {
+// if (config.isPreviewImageSupportEnabled) {
   db = new firestore.Firestore({
     projectId: config.googleProjectId,
     credentials: config.googleApplicationCredentials
   })
 
   images = db.collection(config.firebaseCollectionImages)
-}
+// }
